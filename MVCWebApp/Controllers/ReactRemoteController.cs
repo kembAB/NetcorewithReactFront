b1bf11c8521d3f78fae4b7adf0f24d488cdf2351
abel magicho <32877794+kembAB@users.noreply.github.com>
2022-10-02 18:03:38 +0200

    ReactRemote react controller update

--- MVCWebApp/Controllers/ReactRemoteController.cs
+++ MVCWebApp/Controllers/ReactRemoteController.cs
@@ -1,118 +1,122 @@
-﻿//using Microsoft.AspNetCore.Authorization;
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.EntityFrameworkCore;
-//using Microsoft.EntityFrameworkCore.Internal;
-//using MVCWebApp.Data;
-//using MVCWebApp.Models.Person;
-//using MVCWebApp.Models.Person.ViewModels;
-//using System.Linq;
-//using System.Text.Json;
-//using System.Text.Json.Serialization;
-//using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using MVCWebApp.Data;
+using MVCWebApp.Models.Person;
+using MVCWebApp.Models.Person.ViewModels;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
 
-//namespace MVCWebApp.Controllers
-//{
-//    public class ReactRemoteController : Controller
-//    {
-//        [Route("api/Person/{id}")]
-//        [ApiController]
-//        [RequireHttps]
-//        public class PeopleAPIController : ControllerBase
-//        {
-//          public  readonly ApplicationDbContext _dbContext;
+namespace MVCWebApp.Controllers
+{
+    public class ReactRemoteController : Controller
+    {
+        JsonSerializerOptions jsonSerializerOptions = new()
+        {
+            ReferenceHandler = ReferenceHandler.Preserve
 
-//            public PeopleAPIController(ApplicationDbContext context)
-//            {
-//                _dbContext = context;
-//            }
+        };
 
-//            [HttpGet]
-//            //public JsonResult GetPeople(string? searchString = null)
-//            //{
 
-//            //    CreatePersonViewModel viewModel = new CreatePersonViewModel(_dbContext, searchString);
-//            //    return new JsonResult(viewModel.SelectedPeopleData.ConvertAll(p => {
-//            //        var s = p.StringifyValues;
-//            //        return new { id = s[0], name = s[1], phonenumber = s[2], city = s[3], languages = s[4] };
-//            //    }));
+        ApplicationDbContext dbContext;
+        public ReactRemoteController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
 
-//                //return new JsonResult(await _dbContext.People.ToListAsync());
-//            }
 
-//            [HttpGet("{id}")]
-//            public async Task<ActionResult<Person>> GetPerson(int id)
-//            {
-          
-//        var person = await _dbContext.People.FindAsync(id);
 
-//                if (person == null)
-//                {
-//                    return NotFound();
-//                }
+       
+        [AllowAnonymous]
+        [Route("api/Person/{id}")]
+        public JsonResult GetPeople()
+        {
 
-//                return person;
-//            }
+            var dbResult = dbContext.People.Include(p => p.City).Include(p => p.City.Country).Include(p => p.PersonLanguages);
 
-//            // PUT: api/PeopleAPI/5
-//            // To protect from overposting attacks, enable the specific properties you want to bind to, for
-//            // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-//            [HttpPut("{id}")]
-//            public async Task<IActionResult> PutPerson(int id, Person person)
-//            {
-//                if (id != person.ID)
-//                {
-//                    return BadRequest();
-//                }
+            
+                List<Person> PeopleCleanedUp = new List<Person>();
+            foreach (Person p in dbResult)
+            {
+                PeopleCleanedUp.Add(new Person(p));
+            }
 
-//                _dbContext.Entry(person).State = EntityState.Modified;
+            var jsoned = Json(PeopleCleanedUp, jsonSerializerOptions);
+            return jsoned;
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Person>> GetPerson(int id)
+        {
+            var person = await dbContext.People.FindAsync(id);
 
-//                try
-//                {
-//                    await _dbContext.SaveChangesAsync();
-//                }
-//                catch (DbUpdateConcurrencyException)
-//                {
-//                    if (!PersonExists(id))
-//                    {
-//                        return NotFound();
-//                    }
-//                    else
-//                    {
-//                        throw;
-//                    }
-//                }
+            if (person == null)
+            {
+                return NotFound();
+            }
 
-//                return NoContent();
-//            }
+            return person;
+        }
 
-//            [HttpPost]
-//            public async  Task<ActionResult<Person>> PostPerson(Person person)
-//            {
-//                //_dbContext.People.Add(person);
-//                //await _dbContext.SaveChangesAsync();
+        
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPerson(int id, Person person)
+        {
+            if (id != person.ID)
+            {
+                return BadRequest();
+            }
 
-//                 return CreatedAtAction("GetPerson", new { id = person.ID }, person);
-//            }
+            dbContext.Entry(person).State = EntityState.Modified;
 
-//            [HttpDelete("{id}")]
-//            public async Task<ActionResult<Person>> DeletePerson(int id)
-//            {
-//                var person = await _dbContext.People.FindAsync(id);
-//                if (person == null)
-//                {
-//                    return NotFound();
-//                }
+            try
+            {
+                await dbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PersonExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
 
-//                _dbContext.People.Remove(person);
-//                await _dbContext.SaveChangesAsync();
+            return NoContent();
+        }
 
-//                return person;
-//            }
+        [HttpPost]
+        public async Task<ActionResult<Person>> PostPerson(Person person)
+        {
 
-//            private bool PersonExists(int id)
-//            {
-//                return _dbContext.People.Any(e => e.ID == id);
-//            }
-//        }
-//    }
-//}
+
+            return CreatedAtAction("GetPerson", new { id = person.ID}, person);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Person>> DeletePerson(int id)
+        {
+            var person = await dbContext.People.FindAsync(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            dbContext.People.Remove(person);
+            await dbContext.SaveChangesAsync();
+
+            return person;
+        }
+
+        private bool PersonExists(int id)
+        {
+            return dbContext.People.Any(e => e.ID == id);
+        }
+    }
+}
